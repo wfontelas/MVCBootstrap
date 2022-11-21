698fbe6e9af5ac7e1cf6b670862f5045c970942a
William_Fontelas <williamdevsuporte@gmail.com>
2022-11-21 17:57:27 -0300

    aula 7

---
+++ Net6Bootstrap5CSharp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Net6Bootstrap5CSharp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
