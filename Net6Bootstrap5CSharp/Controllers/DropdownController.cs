d86617234556b9ffb22bf60d7ae67f1b7a163d2c
William_Fontelas <williamdevsuporte@gmail.com>
2022-11-22 17:44:03 -0300

    Table, Dropdown, Card, Carousel,List Group cont...13min

---
+++ Net6Bootstrap5CSharp/Controllers/DropdownController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Net6Bootstrap5CSharp.Controllers
+{
+    public class DropdownController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        
+    }
+}
