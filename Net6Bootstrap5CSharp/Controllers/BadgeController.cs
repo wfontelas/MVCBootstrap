1a36f4ea738822a540353b888521388a163e0332
William Fontelas <williamdevsuporte@gmail.com>
2022-11-21 23:27:45 -0300

    Concluidom a aula 7 sobre Badgers

---
+++ Net6Bootstrap5CSharp/Controllers/BadgeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Net6Bootstrap5CSharp.Controllers
+{
+    public class BadgeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
