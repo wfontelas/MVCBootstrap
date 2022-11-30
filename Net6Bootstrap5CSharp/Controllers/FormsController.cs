b7bba88c2e9f0da1af3d21d567c337b0ae988eb7
William_Fontelas <williamdevsuporte@gmail.com>
2022-11-30 17:44:27 -0300

    Forms 15min video 23

---
+++ Net6Bootstrap5CSharp/Controllers/FormsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Net6Bootstrap5CSharp.Controllers
+{
+    public class FormsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
